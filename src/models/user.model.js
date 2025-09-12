ad000d1ff4f112e7fd327a988d0853e1b6184d04
turabhaider2023 <turab@gmail.com>
2025-09-12 12:02:19 +0530

    commiting on fri

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new Schema(
         lowercase:true,
         trim:true,
     },
-    email:{
+    passord:{
         type:String,
         required:true,
         trim:true,
@@ -59,6 +59,46 @@ const userSchema = new Schema(
 
 
 },{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+
+
+    this.passord = bcrypt.hash(this.passord,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect =  async function (password){
+   return  await bcrypt.compare(password,this.passord)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expireIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id:this._id
+       
+
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expireIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+
+}
 
 
 export const User = mongoose.model("user",userSchema)
