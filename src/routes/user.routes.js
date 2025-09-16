7eeff1387aa4c4e0718c3154f7e9e0d97cde8f53
turabhaider2023 <turab@gmail.com>
2025-09-16 12:34:19 +0530

    commiting on 16th sept

---
+++ src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller";
+const router = Router()
+
+router.route("/Register").post(registerUser)
+
+export default router;
