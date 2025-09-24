cccf6f39005e067408b6992b8ceb6da4a7f3feee
turab <turabw@gmail.com>
2025-09-24 17:36:20 +0530

    commiting on 24th sept

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser} from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/register").
@@ -18,6 +19,12 @@ upload.fields([
     
     
 ,registerUser)
+
+router.route("/login").post(loginUser)
+
+//secure routes
+
+router.route("/logout").post(verifyJWT,logoutUser)
 
 
 export default router;
